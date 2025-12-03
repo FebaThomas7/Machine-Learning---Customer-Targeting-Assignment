81421e6ac1f6d116cea6ba1ca367548402157a07
Feba Thomas <106992437+FebaThomas7@users.noreply.github.com>
2025-12-03 14:22:45 +0400

    ML Assigment Code.py

--- ML Assigment Code.py
+++ ML Assigment Code.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.preprocessing import MinMaxScaler
 from google.colab import files
 uploaded = files.upload()
 
@@ -21,6 +21,24 @@ y = data['new_contract_this_campaign']
 X = data.drop(columns=['new_contract_this_campaign'])
 
 print("Target distribution:\n", y.value_counts())
+X = pd.get_dummies(X, drop_first=False)
+
+# fill missing values
+X = X.fillna(X.mean())
+
+# Scale features
+scaler = MinMaxScaler()
+X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+
+print("Sample of scaled features:\n", X_scaled.head())
+
+
+
+
+
+
+
+
 
 
 
