be301c9cb0f1a7f044b7f8c0e1b5a8dc45949051
Feba Thomas <106992437+FebaThomas7@users.noreply.github.com>
2025-12-03 04:07:10 +0400

    ML Assigment Code.py

--- ML Assigment Code.py
+++ ML Assigment Code.py
@@ -14,3 +14,13 @@ if 'customer_id' in data.columns:
 print(data.head())
 print("Columns:", data.columns)
 print("Missing values per column:\n", data.isnull().sum())
+if data['new_contract_this_campaign'].dtype == object:
+    data['new_contract_this_campaign'] = data['new_contract_this_campaign'].map({'yes': 1, 'no': 0})
+
+y = data['new_contract_this_campaign']
+X = data.drop(columns=['new_contract_this_campaign'])
+
+print("Target distribution:\n", y.value_counts())
+
+
+
